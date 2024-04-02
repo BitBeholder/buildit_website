74909cc72751a628f1d0e813188aa4689c3f1e2e
bitbeholder <e.sario24@gmail.com>
2024-04-02 06:16:12 +0300

    First version

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('next_event_button');
+
+    button.addEventListener('click', function(event) {
+        window.location.href = 'https://lu.ma/0vf6vvpx'; // Redirect to the specified URL
+    });
+});
