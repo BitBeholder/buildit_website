ae5e7d46d82ede062a2e523a35a6342907f4e1cd
Ernesti Sario <56239254+BitBeholder@users.noreply.github.com>
2024-04-09 13:57:21 +0300

    edits

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('next_event_button');
 
     button.addEventListener('click', function(event) {
-        window.location.href = 'https://lu.ma/0vf6vvpx'; // Redirect to the specified URL
+        window.location.href = 'https://lu.ma/pz2swz0i'; // Redirect to the specified URL
     });
 });
 
